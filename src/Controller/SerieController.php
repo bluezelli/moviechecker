97158ed80eff6986947823cbb108b247d1ae0c49
bluezelli <96504071+bluezelli@users.noreply.github.com>
2023-04-19 05:39:57 -0700

    insert gemaakt

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -2,17 +2,60 @@
 
 namespace App\Controller;
 
+use App\Entity\Genre;
+use App\Entity\Serie;
+use App\Repository\EpisodeRepository;
+use App\Repository\GenreRepository;
+use App\Repository\SerieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SerieController extends AbstractController
 {
-    #[Route('/serie', name: 'app_serie')]
-    public function index(): Response
+    #[Route('/home', name: 'categor')]
+    public function index(GenreRepository $genreRepository): Response
     {
+        $genres = $genreRepository->findAll();
+
         return $this->render('serie/index.html.twig', [
-            'controller_name' => 'SerieController',
+            'genres'=>$genres
         ]);
     }
+    #[Route('/serie/{id}', name: 'serieshower')]
+    public function serieShower(SerieRepository $serieRepository, Genre $genreclass): Response
+    {
+        $serie = $serieRepository->findBy(['Genre'=>$genreclass]);
+        return $this->render('serie/serieshow.html.twig', [
+            'serie'=> $serie
+            ]);
+    }
+    #[Route('/episode/{id}', name: 'episode')]
+    public function episodes(EpisodeRepository $episodeRepository, Serie $serieclass): Response
+    {
+        $episode = $episodeRepository->findBy(['serie'=> $serieclass]);
+        return $this->render('serie/episode.html.twig', [
+            'episode'=> $episode
+        ]);
+    }
+
+    #[Route('/insert/{id}', name: 'insert')]
+    public function insert(Request $request, EntityManagerInterface $entityManager)
+    {
+        $genre = new Genre();
+        $form = $this->createForm(Genre::class, $genre);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $genre = $form->getData();
+            $entityManager->persist($genre);
+            $entityManager->flush();
+            $this->addFlash('warning', 'succesfully added');
+            $this->redirectToRoute('home');
+
+        }
+
+    }
+
 }
